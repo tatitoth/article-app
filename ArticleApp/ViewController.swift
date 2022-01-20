f661cc59adcf366154acc3c8d4d75ee725e112a6
Tatiane Toth Pimentel <tatiane.toth@gmail.com>
2022-01-20 19:59:12 -0300

    Initial Commit

---
+++ ArticleApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ArticleApp
+//
+//  Created by Tatiane Toth Pimentel on 20/01/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
